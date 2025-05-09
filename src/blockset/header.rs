4e6450ec91483b8fc61b042faa441385b95d91cb
Marco Bizzaro <51394894+snqre@users.noreply.github.com>
2025-05-09 11:59:11 +0200

    add where clause support

--- src/blockset/header.rs
+++ src/blockset/header.rs
@@ -4,6 +4,7 @@ pub struct Header {
     pub r#type: syn::Type,
     pub r#type_generics: Option<syn::Generics>,
     pub r#impl_generics: Option<syn::Generics>,
+    pub r#where: Option<syn::WhereClause>
 }
 
 impl Header {
@@ -12,8 +13,9 @@ impl Header {
         let r#type: &syn::Type = &self.r#type;
         let r#type_generics: Option<&syn::Generics> = self.r#type_generics.as_ref();
         let r#impl_generics: Option<&syn::Generics> = self.r#impl_generics.as_ref();
+        let r#where: Option<&syn::WhereClause> = self.r#where.as_ref();
         quote::quote! {
-            impl #r#impl_generics #r#type #r#type_generics {
+            impl #r#impl_generics #r#type #r#type_generics #r#where {
                 #(#methods)*
             }
         }
@@ -23,11 +25,12 @@ impl Header {
         let r#type: &syn::Type = &self.r#type;
         let r#type_generics: Option<&syn::Generics> = self.r#type_generics.as_ref();
         let r#impl_generics: Option<&syn::Generics> = self.r#impl_generics.as_ref();
+        let r#where: Option<&syn::WhereClause> = self.r#where.as_ref();
         let r#trait: &syn::Ident = &trait_block.r#trait;
         let r#trait_generics: Option<&syn::Generics> = trait_block.r#trait_generics.as_ref();
         let block: &syn::Block = &trait_block.block;
         quote::quote! {
-            impl #r#impl_generics #r#trait #r#trait_generics for #r#type #r#type_generics
+            impl #r#impl_generics #r#trait #r#trait_generics for #r#type #r#type_generics #r#where
             #block
         }
     }
@@ -40,11 +43,13 @@ impl syn::parse::Parse for Header {
         let r#impl_generics: Option<syn::Generics> = stream.parse().ok();
         let r#type: syn::Type = stream.parse()?;
         let r#type_generics: Option<syn::Generics> = stream.parse().ok();
+        let r#where: Option<syn::WhereClause> = stream.parse().ok();
         let _ = stream.parse::<syn::Token![;]>()?;
         let header: Self = Self {
             r#type: r#type,
             r#type_generics: r#type_generics,
-            r#impl_generics
+            r#impl_generics: r#impl_generics,
+            r#where: r#where
         };
         Ok(header)
     }
